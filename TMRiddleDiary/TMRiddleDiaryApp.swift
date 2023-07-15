8334983ee8201ef6d9b2d6eab8fdaead6d80cf33
NakaokaRei <reideeplearning@gmail.com>
2023-07-15 19:51:41 +0900

    Initial Commit

---
+++ TMRiddleDiary/TMRiddleDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMRiddleDiaryApp.swift
+//  TMRiddleDiary
+//
+//  Created by NakaokaRei on 2023/07/15.
+//
+
+import SwiftUI
+
+@main
+struct TMRiddleDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
